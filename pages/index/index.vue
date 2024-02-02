eb3976827ff7278f98fb9c10bfb499841c23d93d
leghair <zweizhao@foxmail.com>
2024-02-02 13:31:50 +0800

    feat 首页列表加载处理

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,10 @@
 <template>
   <view class="page">
     <CommonHeader title="AI导游大师"></CommonHeader>
-    <navigator v-for="item in list" :key="item.objectId" class="nav-item"
+    <navigator v-for="(item) of list" :key="item.objectId" class="nav-item flex aic jcsb p-10"
       :url="`/pages/welcome/welcome?id=${item.objectId}`">
-      {{ item.name }}
+      <image class="nav-image" :src="item.introduceImageList[0]" mode="aspectFill"></image>
+      <text>{{item.name}}</text>
     </navigator>
   </view>
 </template>
@@ -11,34 +12,54 @@
 <script lang="ts" setup>
   import { ref } from 'vue'
   import type { Ref } from 'vue'
-  import { onLoad, onShareAppMessage } from '@dcloudio/uni-app'
+  import { onLoad, onShareAppMessage, onReachBottom, onPullDownRefresh } from '@dcloudio/uni-app'
   import lc from '@/static/libs/lc'
   import { loading, unloading, } from '@/services/ui'
 
-  type IndexAttraction = { objectId : string, name : string, }
+  type IndexAttraction = { objectId : string, name : string, introduceImageList : string[] }
 
-  const _page = 0
+  let _page = 0
+  const _size = 10
   const list : Ref<IndexAttraction[]> = ref([])
 
   onLoad(() => {
-    doGetAttraction()
+    getAttractionList()
   })
 
-  async function doGetAttraction() {
+  async function getAttractionList() {
     loading()
     const ret = await lc.read('Attraction', (q : AV.Query<AV.Object>) => {
       q.descending('createdAt')
-      q.limit(10)
-      q.skip(_page * 10)
-      q.select(['name'])
+      q.limit(_size)
+      q.skip(_page * _size)
+      q.select(['name', 'introduceImageList'])
     })
-    list.value = ret.map(i => i.toJSON())
+    list.value = [...list.value, ...ret.map(i => i.toJSON())]
+    _page++
+    uni.stopPullDownRefresh()
     unloading()
   }
 
   onShareAppMessage(() => ({
     title: 'AI导游大师'
   }))
+
+  onPullDownRefresh(() => {
+    list.value = []
+    _page = 0
+    getAttractionList()
+  })
+
+  onReachBottom(() => {
+    getAttractionList()
+  })
 </script>
 
-<style></style>
+<style scoped>
+  .nav-item {}
+
+  .nav-image {
+    width: 80px;
+    height: 80px;
+  }
+</style>
